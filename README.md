3e2a5983065c1cc9fb025915e706f19a9a1a4beb
Jason Prince <jasoncprince@gmail.com>
2022-12-15 05:03:40 -0700

    adding all files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# minion
+# Minion
+
+Click on banana and then move the mouse for the eyes to follow the banana.
---
+++ script.js
@@ -0,0 +1,32 @@
+let eye_ref = document.querySelectorAll(".eye");
+let banana = document.getElementsByClassName("banana");
+let isClicked = false;
+
+const clicking = (e) => {
+  if (e.target.matches(".banana")) {
+    isClicked = !isClicked;
+  }
+};
+
+const move = (e) => {
+  if (isClicked == false) return;
+
+  banana[0].style.left = e.pageX + "px";
+  banana[0].style.top = e.pageY + "px";
+
+  eye_ref.forEach((eye) => {
+    let eyeX = eye.getBoundingClientRect().left + eye.clientWidth / 2;
+    let eyeY = eye.getBoundingClientRect().top + eye.clientHeight / 2;
+
+    var x = e.clientX;
+    var y = e.clientY;
+
+    let radian = Math.atan2(x - eyeX, y - eyeY);
+    let rotationDegrees = radian * (180 / Math.PI) * -1 + 180;
+
+    eye.style.transform = `rotate(${rotationDegrees}deg)`;
+  });
+};
+
+document.addEventListener("click", clicking);
+document.addEventListener("mousemove", move);
